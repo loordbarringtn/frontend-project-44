95c3e72eb7ce9dab49207375179e64d362b2b214
loordbarringtn <quantas@gmail.com>
2024-06-11 00:36:50 +0300

    second part of step six

---
+++ src/games/brainCalcFunction.js
@@ -0,0 +1,7 @@
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  return operators[randomIndex];
+};
+
+export default getRandomOperator;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 import determineEvenOrNot from './games/brainEvenFunction.js';
+import getRandomOperator from './games/brainCalcFunction.js';
 
 const askAndGreetUser = () => {
   console.log('Welcome to the Brain game!');
@@ -14,20 +15,54 @@ const runGame = (gameName) => {
   let instructions;
   let attemptsNumber = 3;
   let randomNumber = 0;
+  let randomNumber1 = 0;
+  let randomNumber2 = 0;
   let correctAnswer = '';
+  let operator = '';
 
   const name = askAndGreetUser();
 
-  if (gameName === 'brain-even') {
-    instructions = 'Answer "yes" if the number is even, otherwise answer "no"';
+  switch (gameName) {
+    case 'brain-calc':
+      instructions = 'What is the result of the expression?';
+      break;
+    case 'brain-even':
+      instructions = 'Answer "yes" if the number is even, otherwise answer "no"';
+      break;
+    default:
+      throw new Error(`Unknown game: ${gameName}`);
   }
 
   console.log(instructions);
 
   while (attemptsNumber > 0) {
-    if (gameName === 'brain-even') {
-      randomNumber = getRandomNumber();
-      correctAnswer = determineEvenOrNot(randomNumber);
+    switch (gameName) {
+      case 'brain-calc':
+        randomNumber1 = getRandomNumber();
+        randomNumber2 = getRandomNumber();
+        operator = getRandomOperator();
+        randomNumber = `${randomNumber1} ${operator} ${randomNumber2}`;
+
+        switch (operator) {
+          case '+':
+            correctAnswer = String(randomNumber1 + randomNumber2);
+            break;
+          case '-':
+            correctAnswer = String(randomNumber1 - randomNumber2);
+            break;
+          case '*':
+            correctAnswer = String(randomNumber1 * randomNumber2);
+            break;
+          default:
+            throw new Error(`Unknown operator: ${operator}`);
+        }
+        break;
+      case 'brain-even':
+        randomNumber = getRandomNumber();
+        correctAnswer = determineEvenOrNot(randomNumber);
+        break;
+      default:
+        throw new Error(`Unknown game: ${gameName}`);
     }
 
     console.log(`Question: ${randomNumber}`);
