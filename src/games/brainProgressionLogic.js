d8fd46bb55961042f6bbf129a985f9bd4864b5bf
loordbarringtn <quantas@gmail.com>
2024-06-14 02:30:33 +0300

    fix#2

--- src/games/brainProgressionLogic.js
+++ src/games/brainProgressionLogic.js
@@ -1,5 +1,6 @@
 const makeArithmeticProgression = (step, membersQuantity) => {
   const result = [];
+  // eslint-disable-next-line no-plusplus
   for (let i = 1; i <= membersQuantity; i++) {
     result.push(i * step);
   }
