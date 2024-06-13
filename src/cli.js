98df05bbb41ee3b65e0fdea7d5d876f8aaa631e3
loordbarringtn <quantas@gmail.com>
2024-06-14 02:25:38 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const askAndGreetUser = () => {
-  console.log('Welcome to the Brain game!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
