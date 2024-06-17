fa8498ea2e8948e2bf6be4a6ef4c27531fa78faf
loordbarringtn <quantas@gmail.com>
2024-06-17 22:39:08 +0300

    no-plusplus fix

--- src/games/brainGCDLogic.js
+++ src/games/brainGCDLogic.js
@@ -5,8 +5,7 @@ const instructions = 'Find the greatest common divisor of given numbers.';
 
 const getDivisors = (number) => {
   const divisors = [];
-  // eslint-disable-next-line no-plusplus
-  for (let i = 1; i <= number; i++) {
+  for (let i = 1; i <= number; i += 1) {
     if (number % i === 0) {
       divisors.push(i);
     }
@@ -21,8 +20,7 @@ const getGCD = (a, b) => {
 
   // Найти наибольший общий делитель путем сравнения массивов
   let greatestCommonDivisor = 1;
-  // eslint-disable-next-line no-plusplus
-  for (let i = divisorsA.length - 1; i >= 0; i--) {
+  for (let i = divisorsA.length - 1; i >= 0; i -= 1) {
     if (divisorsB.includes(divisorsA[i])) {
       greatestCommonDivisor = divisorsA[i];
       break;
